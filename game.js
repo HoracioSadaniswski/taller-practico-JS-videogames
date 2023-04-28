7c622bcb2c0ee2681672ad8ee5cabf249f99530a
Horacio Sadaniswski <rhs9208@gmail.com>
2023-04-28 17:21:50 -0300

    6 - Eventos y botones.

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
 
 let canvasSize;
 let elementSize;
@@ -47,4 +51,40 @@ function startGame () {
             game.fillText(emojis[mapRowsCols[row-1][col]], elementSize * col, elementSize * row);
         }
     } */
+}
+
+window.addEventListener('keydown', moveByKeys);
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+function moveByKeys(event) {
+    switch (event.key) {
+        case 'ArrowUp':
+            moveUp();
+            break;
+        case 'ArrowLeft':
+            moveLeft();
+            break;
+        case 'ArrowRight':
+            moveRight();
+            break;
+        case 'ArrowDown':
+            moveDown();
+            break;
+    }
+}
+
+function moveUp() {
+    console.log('Movimiento hacia Arriba')
+}
+function moveLeft() {
+    console.log('Movimiento hacia izquierda')
+}
+function moveRight() {
+    console.log('Movimiento hacia derecha')
+}
+function moveDown() {
+    console.log('Movimiento hacia abajo')
 }
